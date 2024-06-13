e5b078143bbad5ee36caa4fb3b16ad729037ff2c
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-13 22:58:12 +0530

    free main

---
+++ labJdata.py
@@ -0,0 +1,24 @@
+import time
+import CalcScale
+from labjack import ljm
+try:
+    handle = ljm.openS("T7", "USB", "ANY")
+except ljm.ljm.LJMError:
+  print("connection alrady exist")
+else:
+    print("Connuction made")
+    READ = ljm.constants.READ
+    lifetime=[]
+    try:
+        while True:
+            value = ljm.eReadName(handle, "AIN0")
+            lifetime.append(value)
+            print("AIN0: ", value)
+            percentage = CalcScale.scale(value)
+            print("percentage: ", percentage)
+            length = CalcScale.lenfromhome(percentage)
+            print("len: ", length)
+            time.sleep(1)
+    except KeyboardInterrupt:
+            ljm.close(handle)
+            print("Connuction close")
