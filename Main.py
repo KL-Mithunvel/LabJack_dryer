d34a37225267002814f5728c34531a87e513101c
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-14 22:33:54 +0530

    ch

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import labJdata
 import watch
 import threading
+import plot
 #T1=threading.Thread(target=labJdata.data_fromJ)
 runenv=[]
 T2 = threading.Thread(target=watch.timer, args=(1,runenv))
 T2.start()
 print(T2.join())
 print(runenv)
+plot.plotGraph(runenv)
 print("klm")
