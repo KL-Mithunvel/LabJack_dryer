0420794622c71947d161da470de1cecd7ab78e9f
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-14 22:27:02 +0530

    train

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 import labJdata
+import watch
 import threading
-T1=threading.Thread(target=labJdata.data_fromJ)
-T1.start()
-T1.join()
+#T1=threading.Thread(target=labJdata.data_fromJ)
+runenv=[]
+T2 = threading.Thread(target=watch.timer, args=(1,runenv))
+T2.start()
+print(T2.join())
+print(runenv)
 print("klm")
