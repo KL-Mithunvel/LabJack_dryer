e6d5e7cc9a882f4db9d1f2207a1c9480c0cbd96d
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-20 22:33:23 +0530

    start live

--- watch.py
+++ watch.py
@@ -1,18 +1,1 @@
-import time
-import LJ_Acquire
-import threading
-
-def timer(mins,train):
-
-    x = time.time()
-    sec = 0
-    t_end = time.time() + 60 * mins
-    while time.time() < t_end:
-        # print(time.time()-x)
-        l = LJ_Acquire.read_ai_chl("AIN1")
-        print(sec)
-        sec += 1
-        time.sleep(1)
-        train.append([sec, l])
-
 
