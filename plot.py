e385e8f83166ec258e8df9274a84a82d0c7a6820
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-26 00:23:42 +0530

    start live

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 #[[time,mm],[time,mm]]
@@ -22,4 +24,20 @@ def plotGraph(lt):
 
 
 
+def Live_plotGraph(lt):
+    x = []
+    y = []
+    for i in lt:
+        x.append(i[0])
+        y.append(i[1])
 
+    xpoints = np.array(x)
+    ypoints = np.array(y)
+
+    plt.plot(xpoints, ypoints)
+    plt.xlabel('Time')
+    plt.ylabel('distance in mm')
+
+    plt.show()
+    time.sleep(10)
+    plt.close()
