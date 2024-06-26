aef8c9318f0cccb86f422e9ec44713eec5946ea3
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-06-27 03:17:00 +0530

    bugs fixed and plot 3 start one kiss

--- plot.py
+++ plot.py
@@ -4,40 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 #[[time,mm],[time,mm]]
 
+
+
+
+
+
 def plotGraph(lt):
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+
+    twin1 = ax.twinx()
+    twin2 = ax.twinx()
     x = []
-    y = []
+    y1 = []
+    y2 = []
+    y3 = []
     for i in lt:
         x.append(i[0])
-        y.append(i[1])
-
-    xpoints = np.array(x)
-    ypoints = np.array(y)
-
-    plt.plot(xpoints, ypoints)
-    plt.xlabel('Time')
-    plt.ylabel('distance in mm')
-
-    plt.show()
+        y1.append(i[1])
+        y2.append(i[2])
+        y3.append(i[3])
 
 
 
+    twin2.spines.right.set_position(("axes", 1.2))
 
+    p1, = ax.plot([0, 1, 2], [0, 1, 2], "C0", label="Density")
+    p2, = twin1.plot([0, 1, 2], [0, 3, 2], "C1", label="Temperature")
+    p3, = twin2.plot([0, 1, 2], [50, 30, 15], "C2", label="Velocity")
 
-def Live_plotGraph(lt):
-    x = []
-    y = []
-    for i in lt:
-        x.append(i[0])
-        y.append(i[1])
+    ax.set(xlim=x, ylim=y1, xlabel="Time", ylabel="Distance")
+    twin1.set(ylim=y2, ylabel="Temperature")
+    twin2.set(ylim=y3, ylabel="Weight")
 
-    xpoints = np.array(x)
-    ypoints = np.array(y)
+    ax.yaxis.label.set_color(p1.get_color())
+    twin1.yaxis.label.set_color(p2.get_color())
+    twin2.yaxis.label.set_color(p3.get_color())
 
-    plt.plot(xpoints, ypoints)
-    plt.xlabel('Time')
-    plt.ylabel('distance in mm')
+    ax.tick_params(axis='y', colors=p1.get_color())
+    twin1.tick_params(axis='y', colors=p2.get_color())
+    twin2.tick_params(axis='y', colors=p3.get_color())
+
+    ax.legend(handles=[p1, p2, p3])
 
     plt.show()
-    time.sleep(10)
-    plt.close()
